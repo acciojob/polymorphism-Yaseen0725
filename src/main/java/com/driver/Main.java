da84b52e535bd014d3cc31257722c96b729f7426
Mohd-Yaseen <mohdyaseen2856@gmail.com>
2025-01-28 23:06:06 +0530

    Modified code because class name is incorrect

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,14 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        product p = new product();
+        Product p = new Product();
         int res = p.product(10,20);
         int res1 = p.product(5, 10);
         double res2 = p.product(10.0,20.0);
 
         System.out.println(res + " " + res1 + " " + res2);
     }
-    public static class product{
+    public static class Product{
         public int product(int x, int y){
             return x  * y;
         }
